3e275098d238b79e21d3bee23808f7e7ce7e6ec4
Saber Naseralavi <ssna60@yahoo.com>
2023-07-31 10:08:02 +0330

    modified3

--- 100 Statistical Tests In R.R
+++ 100 Statistical Tests In R.R
@@ -1,8 +1,6 @@
 library(psych)
 library(compOverlapCorr)
 #salaam
-
-#----BOOK
 #100 Statistical Tests In R
 #-------------------------------------------------------------------------------
 
@@ -12,8 +10,6 @@ y <- c( 2.6,  3.1,  2.5,  5.0,  3.6,  4.0,  5.2,  2.8,  3.8)
 cor.test(x,y,method="pearson",alternative="two.sided",conf.level = 0.95)
 
 #-------------------------------------------------------------------------------
-#----BOOK
-
 #Test 2 Spearman rank correlation test
 x <- c(44.4, 45.9, 41.9, 53.3, 44.7, 44.1, 50.7, 45.2, 60.1)
 y <- c( 2.6,  3.1,  2.5,  5.0,  3.6,  4.0,  5.2,  2.8,  3.8)
